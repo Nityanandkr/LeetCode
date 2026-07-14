c1ada3dbf85db6dcda6f5c7881cac9a8fec6ce0d
Nityanandkr <nityanandgupta330@gmail.com>
2026-07-14 23:27:24 +0530

    LC-reverse-integer [Runtime: N/A; Memory: N/A]

--- math/reverse-integer.c
+++ math/reverse-integer.c
@@ -1,18 +1,10 @@
 int reverse(int x) {
-    int rev = 0;
+    long long rev = 0;
 
-    while (x != 0) {
-        int digit = x % 10;
-
-        if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7))
-            return 0;
-
-        if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8))
-            return 0;
-
-        rev = rev * 10 + digit;
+    while (x) {
+        rev = rev * 10 + x % 10;
         x /= 10;
     }
 
-    return rev;
+    return (rev > INT_MAX || rev < INT_MIN) ? 0 : (int)rev;
 }
