ce7910addf1f74cf08e1aedaab648fdde2a2ec1c
Nityanandkr <nityanandgupta330@gmail.com>
2026-07-14 23:29:10 +0530

    LC-reverse-integer [Runtime: N/A; Memory: N/A]

--- math/reverse-integer.rs
+++ math/reverse-integer.rs
@@ -1,14 +1,12 @@
 impl Solution {
-    pub fn reverse(x: i32) -> i32 {
-        let mut x = x;
-        let mut rev = 0;
+    pub fn reverse(mut x: i32) -> i32 {
+        let mut rev: i32 = 0;
 
         while x != 0 {
             let digit = x % 10;
             x /= 10;
 
-            match rev.checked_mul(10)
-                     .and_then(|v| v.checked_add(digit)) {
+            match rev.checked_mul(10).and_then(|v| v.checked_add(digit)) {
                 Some(v) => rev = v,
                 None => return 0,
             }
